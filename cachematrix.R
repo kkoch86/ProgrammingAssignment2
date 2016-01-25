4a86cb49b6467941272fa75f06b939c2eb343d1a
kkoch86 <kkoch86@gmail.com>
2016-01-24 20:32:54 -0800

    First commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## The first function makes a list with methods that set and get a matrix and its inverse in an intrinsic environment variable
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  cachedInv <- NULL ## initialize inverse
+  
+  ## set x in parent env with the desired value, if inverse is already set, get rid of it!
+  set <- function(userValue = matrix()) {
+    x <<- userValue 
+    cachedInv <<- NULL
+  }
+  
+  get <- function() x
+  
+  setInverse <- function(invVal) {
+    cachedInv <<- invVal 
+    return(cachedInv)
+  }
+  
+  getInverse  <- function() cachedInv
+  list(set=set, get=get, setInverse=setInverse, getInverse=getInverse)
 }
 
+## given the list variable from the first function, will first check to see if there's already a cached inverse and return
+## otherwise will attempt to solve its inverse and set/return it
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x = matrix(), ...) {
+  
+  ## let's see if there's something there already
+  calculatedInverse <- x$getInverse() 
+  
+  ##check if there's a cached value AND it's a matrix
+  if(!is.null(calculatedInverse) && is.matrix(calculatedInverse)) { 
+    message("Returned Catched Data")
+    return(calculatedInverse)
+  }
+  
+  ## otherwise get the matrix
+  matrixToSolve <- x$get()  
+  
+  ## try to solve the matrix and catch errors and warnings
+  calculatedInverse <- solve(matrixToSolve)
+  message("Setting the value of inverse to:") 
+  x$setInverse(calculatedInverse)
 }
